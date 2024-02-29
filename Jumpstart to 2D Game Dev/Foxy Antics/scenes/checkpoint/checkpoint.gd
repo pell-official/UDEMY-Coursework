0be6c86e72ef879755672c949f744696f6e49508
pell-official <chandler.prunty@gmail.com>
2024-02-29 15:15:16 -0500

    Daily Commit 2/29/24
    Finished boss and checkpoint!

---
+++ Jumpstart to 2D Game Dev/Foxy Antics/scenes/checkpoint/checkpoint.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+const TRIGGER_CONDITION: String = "parameters/conditions/on_trigger"
+@onready var animation_tree = $AnimationTree
+@onready var sound = $Sound
+
+
+func _ready():
+	
+	SignalManager.on_boss_killed.connect(on_boss_killed)
+
+func on_boss_killed(_p: int):
+	animation_tree[TRIGGER_CONDITION] = true
+	monitoring = true
+	SoundManager.play_clip(sound, SoundManager.SOUND_WIN)
+
+func _on_area_entered(area):
+	pass
